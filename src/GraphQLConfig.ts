9857efc41351f5a034d01a8f2658414e7c31363d
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-09-26 15:38:07 +0200

    Changes

--- src/GraphQLConfig.ts
+++ src/GraphQLConfig.ts
@@ -1,73 +1,74 @@
-import { resolve, dirname } from 'path'
-import { validateConfig, writeConfig } from './utils'
-import { values } from 'lodash'
+import { dirname } from 'path';
+import { values } from 'lodash';
+import { validateConfig, writeConfig } from './utils';
 
-import {
-  GraphQLConfigData,
-} from './types'
+import { GraphQLConfigData } from './types';
 
-import { GraphQLProjectConfig } from './GraphQLProjectConfig'
+import { GraphQLProjectConfig } from './GraphQLProjectConfig';
 
 export class GraphQLConfig {
-  public config: GraphQLConfigData
-  public configPath: string
+  public config: GraphQLConfigData;
+  public configPath: string;
 
-  constructor(
-    config: GraphQLConfigData,
-    configPath: string
-  ) {
-    validateConfig(config)
-    this.config = config
-    this.configPath = configPath
+  constructor(config: GraphQLConfigData, configPath: string) {
+    validateConfig(config);
+    this.config = config;
+    this.configPath = configPath;
   }
 
   get configDir() {
-    return dirname(this.configPath)
+    return dirname(this.configPath);
   }
 
   getProjectConfig(projectName?: string): GraphQLProjectConfig {
-    return new GraphQLProjectConfig(this.config, this.configPath, projectName)
+    return new GraphQLProjectConfig(this.config, this.configPath, projectName);
   }
 
   getConfigForFile(filePath: string): GraphQLProjectConfig | undefined {
-    const { projects } = this.config
+    const { projects } = this.config;
 
     if (!projects || Object.keys(projects).length === 0) {
-      const config = new GraphQLProjectConfig(this.config, this.configPath, undefined)
-      return config.includesFile(filePath) ? config : undefined
+      const config = new GraphQLProjectConfig(
+        this.config,
+        this.configPath,
+        undefined,
+      );
+      return config.includesFile(filePath) ? config : undefined;
     }
 
-    return values(this.getProjects()).find(
-      project => project.includesFile(filePath)
-    ) || undefined
+    return (
+      values(this.getProjects()).find(project =>
+        project.includesFile(filePath),
+      ) || undefined
+    );
   }
 
   getProjectNameForFile(filePath: string): string | undefined {
     const proj = this.getConfigForFile(filePath);
-    return proj && proj.projectName || undefined;
+    return (proj && proj.projectName) || undefined;
   }
 
   getProjects(): { [name: string]: GraphQLProjectConfig } | undefined {
-    const result = {}
-    for (const projectName in (this.config.projects || {})) {
-      result[projectName] = this.getProjectConfig(projectName)
+    const result = {};
+    for (const projectName in this.config.projects || {}) {
+      result[projectName] = this.getProjectConfig(projectName);
     }
     if (Object.keys(result).length === 0) {
-      return undefined
+      return undefined;
     }
-    return result
+    return result;
   }
 
   saveConfig(newConfig: GraphQLConfigData, projectName?: string) {
-    let config
+    let config;
     if (projectName) {
       config = this.config;
       config.projects = config.projects || {};
-      config.projects[projectName] = config.projects[projectName] || {}
-      config.projects[projectName] = newConfig
+      config.projects[projectName] = config.projects[projectName] || {};
+      config.projects[projectName] = newConfig;
     } else {
-      config = newConfig
+      config = newConfig;
     }
-    writeConfig(this.configPath, config)
+    writeConfig(this.configPath, config);
   }
 }
--- src/getGraphQLConfig.ts
+++ src/getGraphQLConfig.ts
@@ -1,19 +1,21 @@
-import { readConfig, validateConfig } from './utils'
-import { findGraphQLConfigFile } from './findGraphQLConfigFile'
-import { GraphQLConfig } from './GraphQLConfig'
-import { GraphQLProjectConfig } from './GraphQLProjectConfig'
+import { readConfig, validateConfig } from './utils';
+import { findGraphQLConfigFile } from './findGraphQLConfigFile';
+import { GraphQLConfig } from './GraphQLConfig';
+import { GraphQLProjectConfig } from './GraphQLProjectConfig';
 
-export function getGraphQLConfig(rootDir: string = process.cwd()): GraphQLConfig {
-  const configPath = findGraphQLConfigFile(rootDir)
-  const config = readConfig(configPath)
-  validateConfig(config)
+export function getGraphQLConfig(
+  rootDir: string = process.cwd(),
+): GraphQLConfig {
+  const configPath = findGraphQLConfigFile(rootDir);
+  const config = readConfig(configPath);
+  validateConfig(config);
 
-  return new GraphQLConfig(config, configPath)
+  return new GraphQLConfig(config, configPath);
 }
 
 export function getGraphQLProjectConfig(
   rootDir?: string,
-  projectName: string | undefined = process.env.GRAPHQL_CONFIG_PROJECT
+  projectName: string | undefined = process.env.GRAPHQL_CONFIG_PROJECT,
 ): GraphQLProjectConfig {
-  return getGraphQLConfig(rootDir).getProjectConfig(projectName)
+  return getGraphQLConfig(rootDir).getProjectConfig(projectName);
 }
