b59ab90d7e5084531a62798da0316b68464ded59
Kamil Kisiela <kamil.kisiela@gmail.com>
2020-04-03 21:16:33 +0200

    Check what happens on website update (#491)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -110,7 +110,7 @@ module.exports = {
           customCss: require.resolve('./src/css/custom.css'),
         },
         sitemap: {
-          cacheTime: 600 * 1000, // 600 sec - cache purge period
+          cacheTime: 600 * 1001, // 600 sec - cache purge period
           changefreq: 'weekly',
           priority: 0.5,
         },
