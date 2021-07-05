02121c04f0886e2c1be47dedffa90dd2f04970ec
Alin Simoc <alin.s@softisfy.com>
2021-07-05 19:12:32 +0300

    root components update

--- website/src/theme/Root.js
+++ website/src/theme/Root.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import {ThemeProvider, Header} from 'the-guild-components';
+import {ThemeProvider, Header, FooterExtended} from '@theguild/components';
 
 // Default implementation, that you can customize
 function Root({children}) {
   return (
-    <>
-      <BrowserOnly>
-        {() => (
-          <ThemeProvider>
-            <Header
-              themeSwitch
-              activeLink={'/open-source'}
-              accentColor="var(--ifm-color-primary)"
-            />
-          </ThemeProvider>
-        )}
-      </BrowserOnly>
+    <ThemeProvider>
+      <Header
+        activeLink={'/open-source'}
+        accentColor="var(--ifm-color-primary)"
+      />
       {children}
-    </>
+      <FooterExtended
+        resources={[
+          {
+            children: 'Introduction',
+            title: 'Get started',
+            href: '/introduction',
+          },
+          {
+            children: 'Loading Config',
+            title: 'Learn about Loading Config',
+            href: '/load-config',
+          },
+        ]}
+      />
+    </ThemeProvider>
   );
 }
 
