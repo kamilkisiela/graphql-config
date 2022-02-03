d44c223cd4b4c946a816fbdd20c551dbd9429ec9
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-02-03 13:39:12 +0100

    add crisp

--- website/src/pages/_document.tsx
+++ website/src/pages/_document.tsx
@@ -12,7 +12,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
             rel="stylesheet"
           />
-          <script async src="https://the-guild.dev/static/crisp.js" />
         </head>
         <body>
           <ColorModeScript initialColorMode="light" />
