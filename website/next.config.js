db99f26d02fb1156e7774983e8e348282dd16ddf
Laurin Quast <laurinquast@googlemail.com>
2022-02-22 16:07:40 +0100

    Update next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -14,6 +14,7 @@ module.exports = withBundleAnalyzer(
     redirects: () => {
       return [
         {
+          permanent: true,
           source: '/legacy',
           destination: '/docs/recipes/migration',
         },
