a6838c70457f09030654405f0df284ea389eccdd
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-08 05:38:06 +0200

    prettier

--- website/src/pages/docs/library/_meta.ts
+++ website/src/pages/docs/library/_meta.ts
@@ -1,7 +1,7 @@
 export default {
-  "load-config": "Loading Config",
-  "extensions": "Extensions",
-  "loaders": "Loaders",
-  "graphql-config": "GraphQLConfig",
-  "graphql-project-config": "GraphQLProjectConfig"
-}
+  'load-config': 'Loading Config',
+  extensions: 'Extensions',
+  loaders: 'Loaders',
+  'graphql-config': 'GraphQLConfig',
+  'graphql-project-config': 'GraphQLProjectConfig',
+};
