b2daca4bea74eacad922757b7f6bcbb8d6cc9bb0
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-10-10 21:14:23 +0200

    GraphQL-Config x Nextra 3 (#1414)

---
+++ website/src/pages/docs/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  index: 'Introduction',
+  installation: 'Installation',
+  user: "I'm a User",
+  library: "I'm a Library Author",
+  migration: 'Migration',
+};
---
+++ website/src/pages/docs/library/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  'load-config': 'Loading Config',
+  extensions: 'Extensions',
+  loaders: 'Loaders',
+  'graphql-config': 'GraphQLConfig',
+  'graphql-project-config': 'GraphQLProjectConfig',
+};
