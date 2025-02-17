12915b541278068e7f31b5783ab744b5006ac5e8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-18 05:46:17 +0700

    new graphql config website (#1614)
    * aa
    
    * aa
    
    * aa
    
    * aa
    
    * aa
    
    * aa
    
    * chore(dependencies): updated changesets for modified dependencies
    
    * fix build command
    
    * fix build command 2
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ website/next.config.ts
@@ -0,0 +1,39 @@
+import { withGuildDocs } from '@theguild/components/next.config';
+
+export default withGuildDocs({
+  redirects: async () =>
+    Object.entries({
+      '/legacy': '/docs/migration',
+      '/docs/user/user-introduction': '/docs',
+      '/docs/user/user-installation': '/docs/installation',
+      '/docs/recipes/migration': '/docs/migration',
+      '/docs/user/user-documents': 'docs/user/documents',
+      '/docs/library/api-graphql-config': '/docs/library/graphql-config',
+      '/docs/library/api-graphql-project-config': '/docs/library/graphql-project-config',
+      '/docs/library/author-extensions': '/docs/library/extensions',
+      '/docs/library/author-load-config': '/docs/library/load-config',
+      '/docs/library/author-loaders': '/docs/library/loaders',
+      '/usage': '/docs/user/usage',
+      '/docs/user/user-usage': '/docs/user/usage',
+      '/docs/user': '/docs/user/usage',
+      '/docs/introduction': '/docs',
+      '/extensions': '/docs/library/extensions',
+      '/introduction': '/docs',
+      '/schema': '/docs/user/schema',
+      '/docs/schema': '/docs/user/schema',
+      '/load-config': '/docs/library/load-config',
+      '/docs/user/user-schema': '/docs/user/schema',
+      '/documents': '/docs/user/documents',
+    }).map(([from, to]) => ({
+      source: from,
+      destination: to,
+      permanent: true,
+    })),
+  output: 'export',
+  env: {
+    SITE_URL: 'https://the-guild.dev/graphql/config',
+  },
+  nextraConfig: {
+    contentDirBasePath: '/docs',
+  },
+});
---
+++ website/src/app/layout.tsx
@@ -0,0 +1,78 @@
+import { FC, ReactNode } from 'react';
+import { ConfigLogo, GitHubIcon, HiveFooter, PaperIcon, PencilIcon, PRODUCTS } from '@theguild/components';
+import { getDefaultMetadata, getPageMap, GuildLayout } from '@theguild/components/server';
+import '@theguild/components/style.css';
+
+const description = PRODUCTS.CONFIG.title;
+const websiteName = 'GraphQL-Config';
+
+export const metadata = getDefaultMetadata({
+  description,
+  websiteName,
+  productName: 'CONFIG',
+});
+
+const RootLayout: FC<{
+  children: ReactNode;
+}> = async ({ children }) => {
+  const logo = <ConfigLogo fill="currentColor" className="h-auto w-8" />;
+  return (
+    <GuildLayout
+      htmlProps={{
+        // Override nav width
+        className: '[&>.light_#h-navmenu-container]:max-w-[1392px]',
+      }}
+      websiteName={websiteName}
+      description={description}
+      logo={logo}
+      layoutProps={{
+        docsRepositoryBase: 'https://github.com/kamilkisiela/graphql-config/tree/master/website',
+        footer: (
+          <HiveFooter
+            logo={
+              <div className="flex items-center gap-3">
+                {logo}
+                <span className="text-2xl/[1.2] font-medium tracking-[-0.16px]">{websiteName}</span>
+              </div>
+            }
+            description={description}
+            items={{
+              resources: [
+                {
+                  children: 'Changelog',
+                  href: '/changelog',
+                  title: 'Changelog',
+                },
+              ],
+            }}
+          />
+        ),
+      }}
+      pageMap={await getPageMap()}
+      navbarProps={{
+        developerMenu: [
+          {
+            href: '/docs',
+            icon: <PaperIcon />,
+            children: 'Documentation',
+          },
+          {
+            href: 'https://the-guild.dev/blog',
+            icon: <PencilIcon />,
+            children: 'Blog',
+          },
+          {
+            href: 'https://github.com/kamilkisiela/graphql-config',
+            icon: <GitHubIcon />,
+            children: 'GitHub',
+          },
+        ],
+      }}
+      lightOnlyPages={['/']}
+    >
+      {children}
+    </GuildLayout>
+  );
+};
+
+export default RootLayout;
